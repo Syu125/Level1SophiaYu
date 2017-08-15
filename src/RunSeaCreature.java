5dd8e27b122db6a330d16bc94204ae9f4fa1c04e
100297 <league@WTS7.attlocal.net>
2017-08-15 11:59:01 -0700

    SummerSlam_Day2

---
+++ src/RunSeaCreature.java
@@ -0,0 +1,9 @@
+
+public class RunSeaCreature {
+public static void main(String[] args) {
+	SeaCreature Spongebob=new SeaCreature("Spongebob");
+	Spongebob.getName();
+	Spongebob.eat();
+	Spongebob.laugh();
+}
+}
