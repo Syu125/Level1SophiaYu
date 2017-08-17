05ccbfe26cb52d7bad40925302ec1f308484245a
100297 <league@WTS7.local>
2017-08-17 11:58:07 -0700

    SummerSlam_Day4

--- src/Smurf.java
+++ src/Smurf.java
@@ -8,30 +8,47 @@
 public class Smurf {
 
 	private String name;
+	private String hatColor;
+	private String gender;
 
 	Smurf(String name) {
 		this.name = name;
 	}
-	public String getName() {
-		return "My name is " + name + " Smurf.";
+		public String getName() {
+		return "My name is " + name + ".";
+		}
+	
+	public void hi(){
+		System.out.println("This is " + name + ".");
 	}
-
+	
 	public void eat() {
-		System.out.println(name + " Smurf is eating Smurfberries.");
+		System.out.println(name + " is eating Smurfberries.");
 	}
 
 	/* Papa Smurf wears a red hat, all the others are white. */
-	public String getHatColor() {
+	void setHatColor(String hatColor){
+		this.hatColor = hatColor;
+		}
+		public String getHatColor() {
 		// 3. Fill in this method
-		return "";	
+			return name + " wears a " + hatColor + " hat.";	
+			}
+	public void hat(){
+		System.out.println(name + " wears a " + hatColor + " hat.");
 	}
-
+	
 	/* Smurfette is the only female Smurf. */
+	void setSmurfGender(String gender){
+		this.gender = gender;
+	}
 	public String isGirlOrBoy() {
 		// 4. Fill in this method
-		return "";
+		return name + " is a " + gender;
 	}
-
+	public void smurfGender(){
+		System.out.println(name + " is a " + gender+ ".");
+	}
 }
 
 
--- src/SmurfsRunner.java
+++ src/SmurfsRunner.java
@@ -3,6 +3,31 @@ public class SmurfsRunner {
 	public static void main(String[] args) {
 		Smurf smurf=new Smurf("Handy Smurf");
 		smurf.getName();
+		smurf.hi();
 		smurf.eat();
+		smurf.setHatColor("white");
+		smurf.hat();
+		smurf.setSmurfGender("boy");
+		smurf.smurfGender();
+			System.out.println();
+		
+		Smurf smurf2=new Smurf("Papa Smurf");
+		smurf2.getName();
+		smurf2.hi();
+		smurf2.eat();
+		smurf2.setHatColor("red");
+		smurf2.hat();
+		smurf2.setSmurfGender("boy");
+		smurf2.smurfGender();
+			System.out.println();
+		
+		Smurf smurf3=new Smurf("Smurfette");
+		smurf3.getName();
+		smurf3.hi();
+		smurf3.eat();
+		smurf3.setHatColor("white");
+		smurf3.hat();
+		smurf3.setSmurfGender("girl");
+		smurf3.smurfGender();
 	}
 }
