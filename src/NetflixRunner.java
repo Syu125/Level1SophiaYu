474dcb1dc79714fe17858d5b24ee315868ef9327
100297 <league@WTS7.local>
2017-08-17 11:58:26 -0700

    SummerSlam_Day4

--- src/NetflixRunner.java
+++ src/NetflixRunner.java
@@ -1,4 +1,10 @@
 
 public class NetflixRunner {
-
+public static void main(String[] args) {
+	Movie m1=new Movie("Guardians of the Galaxy",50);
+	Movie m2=new Movie("Transformers: The Last Knight",25);
+	Movie m3=new Movie("Hacksaw Ridge",41);
+	Movie m4=new Movie("Interstellar",22);
+	Movie m5=new Movie("The Martian",50);
+}
 }
