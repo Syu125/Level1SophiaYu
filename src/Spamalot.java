2d8443c691a7f4382ecef1221aadb432230bb6c7
ThomasTwomey <league@WTS15.attlocal.net>
2017-09-30 15:59:04 -0700

    Spamalot/EasyButton

--- src/Spamalot.java
+++ src/Spamalot.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -15,18 +16,22 @@ import javax.swing.JTextField;
 
 
 
-public class Spamalot {
+public class Spamalot implements ActionListener {
 static final String FAKE_USERNAME = "puffclouds2.0@gmail.com";
 static final String FAKE_PASSWORD = "gmailpuffclouds4life";
 	JFrame frame=new JFrame();
 	JPanel panel=new JPanel();
-	JTextField tf=new JTextField(20);
+	static JTextField tf=new JTextField(20);
 	JButton b1=new JButton();
 	JButton b2=new JButton();
-
+ static String subject="";
+	 static String message="";
+	static String recipient=tf.getText();
 public static void main(String[] args) {
 	Spamalot s=new Spamalot();
 	s.spam();
+	s.sendSpam(recipient, subject, message);
+	
 }
 void spam(){
 	frame.setVisible(true);
@@ -40,7 +45,11 @@ void spam(){
 	panel.add(b2);
 	b2.setVisible(true);
 	b2.setText("GOOD");
+	b1.addActionListener(this);
+	b2.addActionListener(this);
+
 	frame.pack();
+	
 }
 private boolean sendSpam(String recipient, String subject, String content) {
 
@@ -70,6 +79,19 @@ private boolean sendSpam(String recipient, String subject, String content) {
 	} catch (MessagingException e) {
 e.printStackTrace();
 return false;
+	}
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	System.out.println(sendSpam("leagueofamazing@gmail.com","hi","hi"));
+	// TODO Auto-generated method stub
+	if(e.getSource()==b1){
+		subject="Hi, friend";
+		message="I know where you live.";
+		
+	}if(e.getSource()==b2){
+		subject="Congratulations!";
+		message="You just won a free ticket to Disneyland!";
 	}
 }
 
