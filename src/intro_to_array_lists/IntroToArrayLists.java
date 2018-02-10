5fb7a6242f09cf5c627263fe5485061eefe115f5
Syu125 <league@WorthyMini-5.attlocal.net>
2018-02-10 15:57:56 -0800

    GuestBook(finished)/ArrayList(almostComplete)

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,58 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class IntroToArrayLists {
+
 	public static void main(String[] args) {
+		IntroToArrayLists ital = new IntroToArrayLists();
+	ital.run();
+	}
+	void run() {
+		String list = "";
+		int count = 0;
 		//1. Create an array list of Strings
+		ArrayList<String>a = new ArrayList<String>();
 		//   Don't forget to import the ArrayList class
 		
 		//2. Add five Strings to your list
 		
+		for(int j = 0; j<5; j++) {
+	
+		a.add(JOptionPane.showInputDialog("List a type of fruit."));
+		}
 		//3. Print all the Strings using a standard for-loop
-		
+		/*for(int i = 0; i <a.size(); i++) {
+			list += a + "\n" ;
+			if(i == a.size()-1) {
+			JOptionPane.showMessageDialog(null, list);
+			}
+			list = "";
+		}*/
 		//4. Print all the Strings using a for-each loop
-		
+		/*for(String item: a) {
+			list += a + "\n" ;
+			JOptionPane.showMessageDialog(null, list);			
+			list = "";
+		}*/
 		//5. Print only the even numbered elements in the list.
-		
+		for(int i = 0; i <a.size(); i++) {
+			count++;
+		if(count%2==0) {
+			list += a + "\n" ;
+			if(i == a.size()-1) {
+				JOptionPane.showMessageDialog(null, list);
+				}
+				list = "";
+		}else {
+			list="";
+		}
 		//6. Print all the Strings in reverse order.
 		
 		//7. Print only the Strings that have the letter 'e' in them.
 	}
+	}
+	
 }
